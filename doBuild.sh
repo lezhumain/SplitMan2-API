c36fbac10b3322ce0020eaba709a563d33d4be2d
lezhumain <julien.merion@whitespace.co.uk>
2022-10-28 22:51:43 +0100

    add quotes

--- doBuild.sh
+++ doBuild.sh
@@ -4,10 +4,11 @@ function sedi()
 {
   if [ -z "$(uname -a | grep -i darwin)" ]; then
     #linux
-    sed -i .bak $1 $2 $3
+    sed -i.bak "$1" "$2" "$3"
+    #sed -i.bak "s|PROD_IP|$1|g" src/environments/environment.prod.ts
   else
     # macos
-    sed -i".bak" $1 $2 $3
+    sed -i ".bak" "$1" "$2" "$3"
   fi
 }
 

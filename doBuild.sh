6a34d9a669120a59affe10daefa71958083d8794
lezhumain <julien.merion@whitespace.co.uk>
2022-10-28 23:08:47 +0100

    change in save controller

--- doBuild.sh
+++ doBuild.sh
@@ -14,6 +14,6 @@ function sedi()
 
 # change ip authorized
 #sed -i .bak -e "s|http://127.0.0.1:4200|$1|g" src/main/java/com/dju/demo/*Controller.java
-sedi -e "s|http://127.0.0.1:4200|$1|g" src/main/java/com/dju/demo/*Controller.java
+sedi -e "s|http://127.0.0.1:4200|$1|g" src/main/java/com/dju/demo/SaveController.java
 
 docker build -t splitman2api .

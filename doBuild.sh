75f77ac702b22c2bc4ff29ca8f011b2813b339f3
lezhumain <julien.merion@whitespace.co.uk>
2022-10-28 22:08:10 +0100

    sed for macOS

--- doBuild.sh
+++ doBuild.sh
@@ -1,6 +1,18 @@
 #! /bin/bash
 
+function sedi()
+{
+  if [ -z "$(uname -a | grep -i darwin)" ]; then
+    #linux
+    sed -i .bak $@
+  else
+    # macos
+    sed -i".bak" $@
+  fi
+}
+
 # change ip authorized
-sed -i .bak -e "s|http://127.0.0.1:4200|$1|g" src/main/java/com/dju/demo/*Controller.java
+#sed -i .bak -e "s|http://127.0.0.1:4200|$1|g" src/main/java/com/dju/demo/*Controller.java
+sedi -e "s|http://127.0.0.1:4200|$1|g" src/main/java/com/dju/demo/*Controller.java
 
 docker build -t splitman2api .

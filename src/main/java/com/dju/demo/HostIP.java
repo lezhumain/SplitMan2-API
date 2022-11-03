bc60cdd13957a70658fbd046f8071c13cc4cfd7b
lezhumain <julien.merion@whitespace.co.uk>
2022-11-03 00:58:05 +0000

    handle host ip split err, tests with FileService

--- src/main/java/com/dju/demo/HostIP.java
+++ src/main/java/com/dju/demo/HostIP.java
@@ -4,6 +4,8 @@ public class HostIP {
     public static final String HOST_IP = "PROD_IP"; // TODO env ?
 
     public static String getIp() {
-        return HOST_IP.split("//")[1].split(":")[0];
+        return HOST_IP.contains("//")
+                ? HOST_IP.split("//")[1].split(":")[0]
+                : null;
     }
 }
